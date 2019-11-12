efd1eb488004376724f9de0371cf7b4d9221c6da
= <=>
2019-11-12 21:08:43 +0700

    validation server side

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,10 +32,27 @@ module.exports.get = (req, res) => {
 
 module.exports.postCreate = (req, res) => {
     let id = shortid.generate();
+    let phone = req.body.phone;
     let name = req.body.name;
     const user = {
         id: id,
-        name: name
+        name: name,
+        phone:phone
+    }
+    let errors = [];
+    if (!req.body.name) {
+        errors.push('Name is required !');
+    }
+    if (!req.body.phone) {
+        errors.push('Phone is required !');
+    }
+
+    if(errors.length){
+        res.render('users/create',{
+            errors:errors,
+            values:req.body
+        })
+        return;
     }
     db.get('users').push(user).write()
     res.redirect("/users")
