fdb732d4626cf35f82698ab357eacc48bb5a4ddf
= <=>
2019-11-12 12:04:10 +0700

    id create

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const lowdb = require('lowdb')
+const shortid = require('shortid')
 
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
@@ -18,10 +19,18 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-db.defaults({ users:[] })
-  .write()
+db.defaults({
+        users: []
+    })
+    .write()
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+
+
+
+
+
 
 app.get('/', (req, res) => res.render('index', {
     name: 'Dung'
@@ -45,12 +54,21 @@ app.get('/users/search', (req, res) => {
 app.get('/users/create', (req, res) => res.render('users/create'))
 
 app.post('/users/create', (req, res) => {
-    let id = db.get('users')[db.get('users').value().length - 1].id + 1
-    let name = req.body.name
+    let id = shortid.generate();
+    let name = req.body.name;
     const user = {
-        id:id,
-        name:name
+        id: id,
+        name: name
     }
     db.get('users').push(user).write()
     res.redirect("/users")
+})
+
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({
+        id: id
+    }).value();
+    res.render('users/view',{user:user})
+    console.log(user.name);
 })
