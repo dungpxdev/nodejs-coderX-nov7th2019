00153faec1f1671f4fac2420f0059021511cea9d
= <=>
2019-11-21 17:12:20 +0700

    middleware & env

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET);
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 let userRoute = require('./routes/user.route')
 let authRoute = require('./routes/auth.route')
+
+let authMiddleware = require('./middlewares/auth.middleware');
 
 const app = express()
 const port = 3000
@@ -18,9 +22,9 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.use(cookieParser());
+app.use(cookieParser(process.env.SESSION_SECRET));
 
-app.use('/users',userRoute)
+app.use('/users',authMiddleware.requireAuth,userRoute)
 app.use('/auth',authRoute)
 
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,14 @@
 const db = require('../db')
+const md5 = require('md5')
 
 module.exports.login = (req, res) => res.render('auth/login')
 
 module.exports.postLogin = (req, res) => {
     let userName = req.body.username;
-    console.log(userName);
+    let password = req.body.password;
+    console.log(userName+" cong "+password);
+
     let user = db.get('users').find({name:userName}).value();
-    
     if(!user){
         res.render('auth/login',{
             errors:[
@@ -17,10 +19,10 @@ module.exports.postLogin = (req, res) => {
         return;
     }
 
-    let password = req.body.password;
-    console.log(password);
-    let pass = db.get('users').find({password:password}).value();
-    if(password !== pass){
+    let hashedPassword = md5(password);
+    console.log(hashedPassword);
+
+    if(user.password !== hashedPassword){
         res.render('auth/login',{
             errors:[
                 'Wrong Password!'
@@ -29,5 +31,8 @@ module.exports.postLogin = (req, res) => {
         })
         return;
     }
+    res.cookie('userId',user.id,{
+      signed:true
+    })
     res.redirect('/users');
 }
